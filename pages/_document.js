ee068523a6c28d13e2f207d74f1d3045566c6a99
zero.choi <zero.choi@goorm.io>
2023-06-16 12:07:19 +0900

    chore: 불필요한 파일 제거

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="ko">
+			<Head>
+				<meta name="description" content="SW CAMP" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link
+					rel="icon"
+					href="https://statics.goorm.io/images/gds/favi_goorm.svg"
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
