a30d7316caabe24d57e3eabc547df37a24f12c03
김레레레렝 <ray.kim@goorm.io>
2023-07-24 10:35:15 +0000

    fix: 운영 어드민 - 신청자 관리 페이지 constants 변수명 오류 수정

--- view/institution/admin/program/[id]/applicant/ApplicantTable/ApplicantTable.constants.js
+++ view/institution/admin/program/[id]/applicant/ApplicantTable/ApplicantTable.constants.js
@@ -3,26 +3,26 @@ import {
     ErrorCircleIcon,
     TimeIcon,
 } from '@goorm-dev/gds-icons';
-import { PROGRAM_DIVISION, CAMP_TICKET_REVIEW_STATUS } from '@/constants/db';
+import { PROGRAM_DIVISION, CAMP_REVIEW_STATUS } from '@/constants/db';
 
 export const CAMP_TICKET_STATUS_TEXT = {
-    [CAMP_TICKET_REVIEW_STATUS.승인.value]: {
-        text: CAMP_TICKET_REVIEW_STATUS.승인.text,
+    [CAMP_REVIEW_STATUS.승인.value]: {
+        text: CAMP_REVIEW_STATUS.승인.text,
         icon: CheckCircleIcon,
         color: 'success',
     },
-    [CAMP_TICKET_REVIEW_STATUS.거절.value]: {
-        text: CAMP_TICKET_REVIEW_STATUS.거절.text,
+    [CAMP_REVIEW_STATUS.거절.value]: {
+        text: CAMP_REVIEW_STATUS.거절.text,
         icon: ErrorCircleIcon,
         color: 'info',
     },
-    [CAMP_TICKET_REVIEW_STATUS.심사중.value]: {
-        text: CAMP_TICKET_REVIEW_STATUS.심사중.text,
+    [CAMP_REVIEW_STATUS.심사중.value]: {
+        text: CAMP_REVIEW_STATUS.심사중.text,
         icon: TimeIcon,
         color: 'warning',
     },
-    [CAMP_TICKET_REVIEW_STATUS.취소.value]: {
-        text: CAMP_TICKET_REVIEW_STATUS.취소.text,
+    [CAMP_REVIEW_STATUS.취소.value]: {
+        text: CAMP_REVIEW_STATUS.취소.text,
         icon: ErrorCircleIcon,
         color: 'info',
     },
@@ -37,21 +37,21 @@ export const DROPDOWN_MENU = {
         },
         {
             index: 1,
-            key: CAMP_TICKET_REVIEW_STATUS.심사중.key,
-            value: CAMP_TICKET_REVIEW_STATUS.심사중.value,
-            text: CAMP_TICKET_REVIEW_STATUS.심사중.text,
+            key: CAMP_REVIEW_STATUS.심사중.key,
+            value: CAMP_REVIEW_STATUS.심사중.value,
+            text: CAMP_REVIEW_STATUS.심사중.text,
         },
         {
             index: 2,
-            key: CAMP_TICKET_REVIEW_STATUS.승인.key,
-            value: CAMP_TICKET_REVIEW_STATUS.승인.value,
-            text: CAMP_TICKET_REVIEW_STATUS.승인.text,
+            key: CAMP_REVIEW_STATUS.승인.key,
+            value: CAMP_REVIEW_STATUS.승인.value,
+            text: CAMP_REVIEW_STATUS.승인.text,
         },
         {
             index: 3,
-            key: CAMP_TICKET_REVIEW_STATUS.거절.key,
-            value: CAMP_TICKET_REVIEW_STATUS.거절.value,
-            text: CAMP_TICKET_REVIEW_STATUS.거절.text,
+            key: CAMP_REVIEW_STATUS.거절.key,
+            value: CAMP_REVIEW_STATUS.거절.value,
+            text: CAMP_REVIEW_STATUS.거절.text,
         },
     ],
     [PROGRAM_DIVISION.집합형]: [
@@ -62,21 +62,21 @@ export const DROPDOWN_MENU = {
         },
         {
             index: 1,
-            key: CAMP_TICKET_REVIEW_STATUS.승인.key,
-            value: CAMP_TICKET_REVIEW_STATUS.승인.value,
-            text: CAMP_TICKET_REVIEW_STATUS.승인.text,
+            key: CAMP_REVIEW_STATUS.승인.key,
+            value: CAMP_REVIEW_STATUS.승인.value,
+            text: CAMP_REVIEW_STATUS.승인.text,
         },
         {
             index: 2,
-            key: CAMP_TICKET_REVIEW_STATUS.거절.key,
-            value: CAMP_TICKET_REVIEW_STATUS.거절.value,
-            text: CAMP_TICKET_REVIEW_STATUS.거절.text,
+            key: CAMP_REVIEW_STATUS.거절.key,
+            value: CAMP_REVIEW_STATUS.거절.value,
+            text: CAMP_REVIEW_STATUS.거절.text,
         },
         {
             index: 3,
-            key: CAMP_TICKET_REVIEW_STATUS.취소.key,
-            value: CAMP_TICKET_REVIEW_STATUS.취소.value,
-            text: CAMP_TICKET_REVIEW_STATUS.취소.text,
+            key: CAMP_REVIEW_STATUS.취소.key,
+            value: CAMP_REVIEW_STATUS.취소.value,
+            text: CAMP_REVIEW_STATUS.취소.text,
         },
     ],
 };
